0e732cc930462cf815366a6279e092d3e9683254
Maksym Javir <Jestermaxrko@gmail.com>
2018-01-23 01:28:54 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -63,6 +63,8 @@ function checkUserIsSignIn(){
   			document.getElementById("login_btn").style.display = "none";
   			document.getElementById("register_btn").style.display="none";
 
+  			//subscribeToNotifications();
+
   			firebase.database().ref("/users/"+user.uid).once('value').then(function(snapshot){
 				current_user = snapshot.val();
     			getPosts();
@@ -333,3 +335,75 @@ function loadComments(elem){
 }
 
 
+
+/////////////////// NOTIFICATION /////////////////////////////////
+
+
+{
+
+/* ========================
+  Variables
+======================== */
+
+const FIREBASE_AUTH = firebase.auth();
+const FIREBASE_MESSAGING = firebase.messaging();
+const FIREBASE_DATABASE = firebase.database();
+
+const subscribeButton = document.getElementById('subscribe');
+const unsubscribeButton = document.getElementById('unsubscribe');
+const sendNotificationForm = document.getElementById('send-notification-form');
+
+/* ========================
+  Event Listeners
+======================== */
+
+FIREBASE_MESSAGING.onTokenRefresh(handleTokenRefresh);
+
+
+
+/* ========================
+  Functions
+======================== */
+
+function handleTokenRefresh() {
+  return FIREBASE_MESSAGING.getToken().then((token) => {
+    FIREBASE_DATABASE.ref('/tokens').push({
+      token: token,
+      uid: FIREBASE_AUTH.currentUser.uid
+    });
+  });
+}
+
+function checkSubscription() {
+  FIREBASE_DATABASE.ref('/tokens').orderByChild("uid").equalTo(FIREBASE_AUTH.currentUser.uid).once('value').then((snapshot) => {
+    if ( snapshot.val() ) {
+      
+    } else {
+      
+    }
+  });
+}
+
+function subscribeToNotifications() {
+  FIREBASE_MESSAGING.requestPermission()
+    .then(() => handleTokenRefresh())
+    .then(() => checkSubscription())
+    .catch((err) => {
+      console.log("error getting permission :(");
+    });
+}
+
+function unsubscribeFromNotifications() {
+  FIREBASE_MESSAGING.getToken()
+    .then((token) => FIREBASE_MESSAGING.deleteToken(token))
+    .then(() => FIREBASE_DATABASE.ref('/tokens').orderByChild("uid").equalTo(FIREBASE_AUTH.currentUser.uid).once('value'))
+    .then((snapshot) => {
+      const key = Object.keys(snapshot.val())[0];
+      return FIREBASE_DATABASE.ref('/tokens').child(key).remove();
+    })
+    .then(() => checkSubscription())
+    .catch((err) => {
+      console.log("error deleting token :(");
+    });
+}
+}
