0e079ee7b4e164610cc3abe75131ca771cd949e2
Maksym Javir <Jestermaxrko@gmail.com>
2018-01-22 22:47:09 +0200

    Add files via upload

--- scripts/upload.js
+++ scripts/upload.js
@@ -10,8 +10,16 @@ var ctx;
 var is_temp_open = true;
 
 
+
+sendNotification('New Post', {
+						body: 'Тестирование HTML5 Notifications',
+						icon: 'icon.jpg',
+						dir: 'auto'
+						});
 initFirebase();
 checkUserIsSignIn();
+
+
 
 //var ref = new Firebase('application.firebaseio.com/posts');
 
@@ -129,6 +137,9 @@ function loadTemplteToCanvas(elem){
 
 function uploadImage (title) {
 
+
+
+
 	if(current_user){
 
 		const ref = firebase.storage().ref();
@@ -170,6 +181,7 @@ function uploadImage (title) {
 		  	
 		  		firebase.database().ref('posts/').push(post).then(function(){
 
+		  			
 		  			window.location.replace("index.html");	
 		  		});
 
@@ -369,3 +381,40 @@ function showHideTemp(event){
 
 
 
+function sendNotification(title, options) {
+
+	Notification.permission = "granted";
+
+
+	// Проверим, поддерживает ли браузер HTML5 Notifications
+	if (!("Notification" in window)) {
+	alert('Ваш браузер не поддерживает HTML Notifications, его необходимо обновить.');
+	}
+
+	// Проверим, есть ли права на отправку уведомлений
+	else if (Notification.permission === "granted") {
+	// Если права есть, отправим уведомление
+	var notification = new Notification(title, options);
+
+	function clickFunc() { alert('Пользователь кликнул на уведомление'); }
+
+	notification.onclick = clickFunc;
+	}
+
+	// Если прав нет, пытаемся их получить
+	else if (Notification.permission !== 'denied') {
+	Notification.requestPermission(function (permission) {
+	// Если права успешно получены, отправляем уведомление
+	if (permission === "granted") {
+	var notification = new Notification(title, options);
+
+	} else {
+	alert('Вы запретили показывать уведомления'); // Юзер отклонил наш запрос на показ уведомлений
+	}
+	});
+	} else {
+	// Пользователь ранее отклонил наш запрос на показ уведомлений
+	// В этом месте мы можем, но не будем его беспокоить. Уважайте решения своих пользователей.
+}
+
+}
