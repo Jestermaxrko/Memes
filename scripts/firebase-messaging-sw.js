38aa1e63f0bbad629de449d41a7b0e7d2a028289
Maksym Javir <Jestermaxrko@gmail.com>
2018-01-23 16:14:16 +0200

    Delete firebase-messaging-sw.js

--- scripts/firebase-messaging-sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '284229682694'
-
-const messaging = firebase.messaging();
